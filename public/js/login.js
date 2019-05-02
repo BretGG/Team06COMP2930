f602aca08fbf7e7c54e2ff232d6a4d62498c7c73
stellatranthi <43520327+stellatranthi@users.noreply.github.com>
2019-05-02 11:22:47 -0700

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,12 @@
 $(document).ready(() => {
   let print = user => {
-    console.log(JSON.stringify(user));
+    console.log("JSON.stringify(user)");
+    // $("#userInfo").html("User Info: " + JSON.stringify(user));
   };
 
   $("#submit").click(() => {
     console.log("hello");
+    if($("#pass").val().equals($("#cpass").val()))
     $.ajax({
       type: "post",
       url: "/users",
@@ -18,4 +20,20 @@ $(document).ready(() => {
     });
   });
 
+  $("#signin").click(() => {
+    console.log("sign-in");
+    $("#signup").css("border-bottom", "none");
+    $("#signin").css("border-bottom", "2px solid #42A164");
+    $("#signin").css("height", "#signupcon.height");
+    $("#signupcon").css("display", "none");
+    $("#signincon").css("display", "block");
+  });
+
+  $("#signup").click(() => {
+    console.log("sign-up");
+    $("#signin").css("border-bottom", "none");
+    $("#signup").css("border-bottom", "2px solid #42A164");
+    $("#signincon").css("display", "none");
+    $("#signupcon").css("display", "block");
+  });
 });
