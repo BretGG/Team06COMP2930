fb1205f3baa57f8892e963e2fbb0be855b7f1e77
Rose Song <sooominsong@gmail.com>
2019-05-09 15:02:34 -0700

    Connected mine and Jessicas code for socket.io and allowed each user to have their own character visuals.

--- bin/server.js
+++ bin/server.js
@@ -1,12 +1,49 @@
 #!/usr/bin/env node
-
+var playerLimit = 4;
+var currentNoOfPlayers = 0;
 /**
  * Module dependencies.
  */
-
 var app = require("../app");
 var debug = require("debug")("comp2930-team2:server");
 var http = require("http");
+
+var players = {};
+///Code below will be put into different channels in the future to implement another game
+var count = 0;
+// var increaseX = 200;
+// var times = 0;
+const numberOfPlayers = 4;
+const Game1_XYCoordinates = [{
+        x: 110,
+        y: 225,
+        isTaken: false
+    }, {
+        x: 310,
+        y: 225,
+        isTaken: false
+    },
+    {
+        x: 510,
+        y: 225,
+        isTaken: false
+    }, {
+        x: 710,
+        y: 225,
+        isTaken: false
+    }
+];
+
+
+//TODO: //////////////////////////////////
+//1. Implement limited number of max players ..
+//How to get the variable e.g. numberOfPlayers from game.js?
+//emit from game.js doesn't work...=> It can't come from the client side. use namespace instead.
+//2. Give the players different colors..
+//3. Is there way to get the initial spawn location coordinates from game.js?
+//4. Use namespace(multiple channel feature of socket.io) to implement different game
+/////////////////////////////////////////
+
 
 /**
  * Get port from environment and store in Express.
@@ -19,7 +56,9 @@ app.set("port", port);
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
+var server = http.Server(app);
+var io = require('socket.io').listen(server);
+
 
 /**
  * Listen on provided port, on all network interfaces.
@@ -34,19 +73,19 @@ server.on("listening", onListening);
  */
 
 function normalizePort(val) {
-  var port = parseInt(val, 10);
+    var port = parseInt(val, 10);
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
+    if (port >= 0) {
+        // port number
+        return port;
+    }
 
-  return false;
+    return false;
 }
 
 /**
@@ -54,25 +93,25 @@ function normalizePort(val) {
  */
 
 function onError(error) {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
+    if (error.syscall !== "listen") {
+        throw error;
+    }
 
-  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+    var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case "EACCES":
+            console.error(bind + " requires elevated privileges");
+            process.exit(1);
+            break;
+        case "EADDRINUSE":
+            console.error(bind + " is already in use");
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
 }
 
 /**
@@ -80,7 +119,104 @@ function onError(error) {
  */
 
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-  debug("Listening on " + bind);
+    var addr = server.address();
+    var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+    debug("Listening on " + bind);
+}
+
+
+//on the new user connection do the following
+/// /////////
+/// A1
+/// server is up and running
+/// server is made here. 
+/// //////
+
+/// Connected A901
+
+
+
+io.on('connection', function(socket) {
+    console.log('a user connected: ' + socket.id);
+    let x, y, playerNo;
+
+
+    // console.log(Object.keys(io.sockets.sockets));
+    //Make new player by new connection on random x,y coordinates.
+    // if(count+1 <= numberOfPlayers){
+        
+    if (isRoomFull()!= -1){
+        for (let i = 0; i < Game1_XYCoordinates.length; i++){
+            if (!Game1_XYCoordinates[i].isTaken){
+                Game1_XYCoordinates[i].isTaken = true; 
+                playerNo = i;
+              
+              players[socket.id] = {
+                    playerNo: i,
+                    playerId: socket.id,
+                    x:  Game1_XYCoordinates[i].x,
+                    y:  Game1_XYCoordinates[i].y
+                };
+                console.log("new player added");
+                //currentNoOfPlayers++;
+                printPlayers(Game1_XYCoordinates);
+
+                break;
+            }
+        }
+    } 
+    else {
+        console.log("room is full");
+        delete players[socket.id];
+    }
+    // send the players array object to the new player
+    socket.emit('currentPlayers', players);
+    // update all other players of the new player
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+
+    //user disconnect using socket.io function.
+    socket.on('disconnect', function() {
+        console.log('count : ' + playerNo + ' / user disconnected: ', socket.id);
+        if (Game1_XYCoordinates[playerNo]!=undefined) {
+            Game1_XYCoordinates[playerNo].isTaken = false;
+            delete players[socket.id];
+            printPlayers(Game1_XYCoordinates);
+            io.emit('disconnect', socket.id);
+        } else {
+            console.log("no disconnection");
+        }
+    });
+
+    // when a player moves, update the player data
+    //then sends it to the other players in socket.broadcast.emit*'playermoved'
+    socket.on('playerMovement', function(movementData) {
+        if(players[socket.id]!=undefined){
+            players[socket.id].x = movementData.x;
+            players[socket.id].y = movementData.y;
+        }
+        // emit a message to all players about the player that moved
+        socket.broadcast.emit('playerMoved', players[socket.id]);
+    });
+
+});
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function printPlayers(coordinates) {
+    for (let i = 0; i < coordinates.length; i++) {
+        console.log(coordinates[i].isTaken);
+    }   
+}
+
+
+function isRoomFull(){
+    for(let i=0; i<Game1_XYCoordinates.length; i++){
+      if(!Game1_XYCoordinates[i].isTaken){
+        return i; //false
+      }
+    }
+    return -1; //true
 }
--- game/Public/js/game.js
+++ game/Public/js/game.js
@@ -1,36 +1,111 @@
-var question,  numOfPlayers;
-var smallPlatform, bigPlatform, p1, p2, p3, p1cursor, p2cursor, p3cursor;
+var question;
+var myPlayerSocket;
+var thisSprite;
+var playersArray = [];
+//number of players in the room.
+var noOfPlayers;
+var noOfPlayersText;
+
+
+var smallPlatform, bigPlatform;
 var platform1, platform2, platform3;
 var cursors;
-//flashcard class that creates 
 
-function Flashcard(text, answer) {
-    this.text = text,
-    this.answer = answer,
 
-    function getText(){
-        return this.text;
+var player1, player2, player3;
+//To be changed to true if they are connected to server.
+player1 = false;
+player2 = false;
+player3 = false;
+
+
+///////////////////////////////////////////////////////////////////////////////
+//////////////Should probably be in a different file///////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+function qandA(question, answer) {
+    this.question = question;
+    this.answersList = [answer];
+
+    function getQuestion() {
+        return this.question;
     }
-
-   function getAnswer() {
-        return this.answer;
+    /**
+    Adds a dummy answer card to the original array.
+    Goes through a while loop, and pushes a random index 
+    from the original array into the temp array,
+    Removes that index that is pushed into the temp array.
+    Object pushed into the array is removed from the original array.
+    While loop repeats until all objects are randomly selected and
+    pushed into the new array. 
+    Then the original Array is reassigned to temp array.
+    **/
+    function addDummy(dummy) {
+        answersList.push(dummy);
+        let tempArray = [];
+        while (this.answersList.length != 0) {
+            let randIndex = Math.floor(Math.random() * this.answersList.length);
+            tempArray.push(answersList[randIndex]);
+            answersList.splice(randomIndex, 1);
+        }
+        this.answersList = tempArray;
     }
-}    
-    
-    
+}
+////////////////////////////////////////////////
+function answers(text, correct) {
+    this.answer = text;
+    this.correct = correct;
+}
+/**
+ player object skeleton:
+ 
+    this.name = name;
+    this.active = active;
+    this.score = 0;
+    this.platform;
+    this.spriter;
+    this.   
+}
+**/
+
+function startGame() {
+    p1 = player("rose", true);
+    p2 = player("jessica", true);
+    p2 = player("hannah", true);
+    p4 = player("stella", true);
+    playerList = {
+        p1,
+        p2,
+        p3,
+        p4
+    };
+    let card1 = answers("1", false);
+    let card2 = answers("2", false);
+    let card3 = answers("3", false);
+    let card4 = answers("4", true);
+    questionAnswer = new qandA("What is 2 + 2", card4);
+    questionAnswer.addDummy(card1);
+    questionAnswer.addDummy(card2);
+    questionAnswer.addDummy(card3);
+}
+
+
+////////////////////////////////
+
 var config = {
     type: Phaser.AUTO,
     scale: {
         mode: Phaser.Scale,
         parent: 'phaser-example',
         autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 1600,
-        height: 1200
+        width: 800,
+        height: 600
     },
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 0 },
+            gravity: {
+                y: 0
+            },
             debug: false
         }
     },
@@ -40,84 +115,182 @@ var config = {
         update: update
     }
 };
+var game = new Phaser.Game(config);
+
 
 function preload() {
     // this.load.setBaseURL("http://labs.phaser.io");
-        this.load.image("sky", "assets/backgrounds/sky.png");
-    this.load.image("peach", "assets/character/peach.png");
-    this.load.image("cake", "assets/character/cake.png");
-    this.load.image("platform", "assets/character/platform.png");
-    this.load.image("scroll", "assets/character/scroll.png");
-    this.load.image("card", "assets/character/empty_card.png");
+    this.load.image("sky", "../assets/backgrounds/sky.png");
+    this.load.image("cake", "../assets/character/cake.png");
+    this.load.image("p1", "../assets/character/dratini.png");
+    this.load.image("p2", "../assets/character/eevee.png");
+    this.load.image("p3", "../assets/character/pikachu.png");
+    this.load.image("p4", "../assets/character/rapidash.png");
 
+    this.load.image('otherPlayer', 'assets/character/cake.png');
+    this.load.image("platform", "../assets/character/platform.png");
+    this.load.image("scroll", "../assets/character/scroll.png");
+    this.load.image("card", "../assets/character/empty_card.png");
 
-}
+};
 
 function create() {
-    // setting the backgroubnd image
-    this.add.image(000, 00, "sky").setOrigin(0).setDisplaySize(1600, 1200);
-    //invisible platforms for players to stand on.
-    this.add.image(800, 200, 'scroll').setScale(.3);
-
+    /// connected A901
+    this.socket = io();
+    this.opponentPlayers = this.physics.add.group();
     cursors = this.input.keyboard.createCursorKeys();
-    p1 = this.add.image(200, 350, 'cake').setScale(0.5);
+    var self = this;
+    // setting the backgroubnd image
+    this.add.image(000, 00, "sky").setOrigin(0).setDisplaySize(800, 600);
+    //invisible platforms for players to stand on.
+    this.add.image(400, 100, 'scroll').setScale(.15); // p1 = this.add.image (300,300,'cake').setScale(0.25);
 
-    var card1 = this.add.image(330, 1100, 'card').setScale(.9);
-    card1.setInteractive();
-    card1.on('clicked', clickHandler, this);
+    this.socket.on('currentPlayers', function(players) {
+        console.log(players);
+        Object.keys(players).forEach(function(id) {
+            playersArray = players;
+        //    console.log(id);
+            if (players[id].playerId === self.socket.id) {
+                //global variable for own character
+                playersArray[self.socket.id].sprite = thisSprite;
+                //global variable to save the self.socket.id
+                myPlayerSocket = self.socket.id;
+                thisSprite = myCharacterChooser(self, playersArray[id]);
 
-    var card2 = this.add.image(830, 1100, 'card').setScale(.9);
-    card2.setInteractive();
-    card2.on('clicked', clickHandler, this);
+            } else {
+                let opponentGraphic = opponentGraphicChooser(self, playersArray[id]);
+                //updateSpriteGroup(self.opponentPlayers, opponentGraphic, playersArray[id]);
+            }
+        });
+    });
 
-    var card3 = this.add.image(1330, 1100, 'card').setScale(.9);
-    card3.setInteractive();
-    card3.on('clicked', clickHandler, this);
+    ///A6 receives player object in a call back function and calls at other players. to show it. 
+    this.socket.on('newPlayer', function(newOpponentInfo) {
+        console.log("inside newplayer"); //never ran
+        playersArray[newOpponentInfo.playerId] = newOpponentInfo;
+        let graphic = opponentGraphicChooser(self, newOpponentInfo);
+      //  updateSpriteGroup(self.opponentPlayers, graphic, newOpponentInfo);
+    });
 
+    this.socket.on('disconnect', function(playerId) {
+        self.opponentPlayers.getChildren().forEach(function(opponent) {
+            if (playerId === opponent.playerId) {
+                playersArray[playerId].sprite.destroy();
+                delete playersArray[playerId];
+            }
+        });
+    });
+    
+    this.socket.on('playerMoved', function(movementData) {
+        //  console.log("inside plyaer moved");
+        self.opponentPlayers.getChildren().forEach(function(opponent) {
+            //console.log("inside plyaer moved");
+
+            if (movementData.playerId === opponent.playerId) {
+                // otherPlayer.setRotation(playerInfo.rotation);
+                //     console.log("inside plyaer moved");
+                opponent.setPosition(movementData.x, movementData.y);
+                //console.log("inside plyaer moved");
+            }
+        });
+    });
+    
+
+    var card1 = this.add.image(165, 550, 'card').setScale(.45);
+    card1.setInteractive().on('clicked', clickHandler, this);
+
+    var card2 = this.add.image(415, 550, 'card').setScale(.45);
+    card2.setInteractive().on('clicked', clickHandler, this);
+
+    var card3 = this.add.image(670, 550, 'card').setScale(.45);
+    card3.setInteractive().on('clicked', clickHandler, this);
 
     //  If a Game Object is clicked on, this event is fired.
     //  We can use it to emit the 'clicked' event on the game object itself.
-    this.input.on('gameobjectup', function (pointer, gameObject)
-    {
+    this.input.on('gameobjectup', function(pointer, gameObject) {
         gameObject.emit('clicked', gameObject);
     }, this);
 
-    //  Display the game stats
-  
+}
 
-
+function scoreAndPlayer() {
+    scoreText = this.add.text(16, 16, 'score: 0', {
+        fontSize: '32px',
+        fill: '#000'
+    });
 
 }
 
 ////////////////////////click handler////////////////////
-function clickHandler (box)
-{ 
-    p1.y += 32;
+function clickHandler(box) {
+    console.log("card clicked");
+    // player.y += 32;
+    if (thisSprite) {
+        thisSprite.y += 32;
+    }
 }
 ////////////////////////////////////////////////////////
-
-
-
-function update(){
- if (this.input.keyboard.checkDown(cursors.left, 250))
-    {
-        p1.x -= 32;
+///A12 it emits new X and Y coordinates 
+function update() {
+    if (thisSprite) {
+        if (this.input.keyboard.checkDown(cursors.left, 250)) {
+            thisSprite.x -= 32;
+        } else if (this.input.keyboard.checkDown(cursors.right, 250)) {
+            thisSprite.x += 32;
+        }
+        //  Vertical movement every 150ms
+        if (this.input.keyboard.checkDown(cursors.up, 150)) {
+            thisSprite.y -= 32;
+        } else if (this.input.keyboard.checkDown(cursors.down, 150)) {
+            thisSprite.y += 32;
+        }
+        this.socket.emit('playerMovement', {
+            x: thisSprite.x,
+            y: thisSprite.y
+        });
     }
-    else if (this.input.keyboard.checkDown(cursors.right, 250))
-    {
-        p1.x += 32;
-    }
-
-    //  Vertical movement every 150ms
-    if (this.input.keyboard.checkDown(cursors.up, 150))
-    {
-        p1.y -= 32;
-    }
-    else if (this.input.keyboard.checkDown(cursors.down, 150))
-    {
-        p1.y += 32;
-    }
+    //console.log(playersArray[myPlayerSocket]);
 }
 
+function opponentGraphicChooser(self, opponentInfo) {
+    let playerVisual;
+    switch (opponentInfo.playerNo) {
+        case 1:
+            playerVisual = self.add.sprite(opponentInfo.x, opponentInfo.y, 'p2').setScale(0.25);
+            break;
+        case 2:
+            playerVisual = self.add.sprite(opponentInfo.x, opponentInfo.y, 'p3').setScale(0.25);
+            break;
+        case 3:
+            playerVisual = self.add.sprite(opponentInfo.x, opponentInfo.y, 'p4').setScale(0.25);
+            break;
+        default:
+            playerVisual = self.add.sprite(opponentInfo.x, opponentInfo.y, 'p1').setScale(0.25);
+    }
+    //add sprite attribute to access srpite in the array. 
+    opponentInfo.sprite = playerVisual;
+    //adding attributes to the sprite.
+    playerVisual.playerId = opponentInfo.playerId;
+    playerVisual.playerNo = opponentInfo.playerNo;
+   // opponentPlayers.add(picture);
+    self.opponentPlayers.add(playerVisual);
+    return playerVisual;
+}
 
-var game = new Phaser.Game(config);
+function myCharacterChooser(self, playerInfo) {
+    let picture;
+    switch (playerInfo.playerNo) {
+        case 1:
+            picture = self.physics.add.image(playerInfo.x, playerInfo.y, 'p2').setScale(0.25);
+            break;
+        case 2:
+            picture = self.physics.add.image(playerInfo.x, playerInfo.y, 'p3').setScale(0.25);
+            break;
+        case 3:
+            picture = self.physics.add.image(playerInfo.x, playerInfo.y, 'p4').setScale(0.25);
+            break;
+        default:
+            picture = self.physics.add.image(playerInfo.x, playerInfo.y, 'p1').setScale(0.25);
+    }
+    return picture;
+}
