640f0bdd7529306aab90b92923abc55ee28b3ec2
Stella Tran <stellatranthi@hotmail.com>
2019-05-10 13:03:05 -0700

    Created dummy function that adds username to memberpool div of gameLobby.HTML

---
+++ public/js/gameLobby.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+    var max = 4;
+    var count = 0;
+    var username = ["Stella", "Jessica", "Rose", "Hannah", "Bret"]
+
+    $('#memberpool').click(()=>{
+      console.log("k");
+      if(count < 4){
+        const member = document.createElement('div');
+        member.setAttribute('class', 'member');
+        member.textContent = username[count];
+        $('#memberpool').append(member);
+        count++;  
+      }  
+    })
+  });
