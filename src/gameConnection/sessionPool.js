cc733f5745379478d3573f05ded8fa2b63c00dc4
BretGG <Bret.G@hotmail.ca>
2019-05-13 21:01:53 -0700

    Created function that returns a promise on updating pool information, using messaging to update information instead of object references

--- src/gameConnection/sessionPool.js
+++ src/gameConnection/sessionPool.js
@@ -14,16 +14,19 @@ class SessionPool {
     this.poolId = poolId;
     this.poolLimit = poolLimit;
     this.currentSessionsCount = 0;
+    this.full = false;
     this.sessions = [];
 
-    setInterval(() => {
-      console.log(isMainThread);
-    }, 1000);
+    // setInterval(() => {
+    //   console.log(isMainThread);
+    // }, 1000);
 
     parentPort.on("message", message => this.handleParentRequest(message));
+    console.log("New pool: " + this.poolId);
   }
 
-  registerSession(session) {
+  // Add a new session to this pool;
+  registerSession(sessionInfo) {
     if (this.sessions.length === this.poolLimit) return null;
     else {
       // TODO: add session to pool
@@ -46,6 +49,7 @@ class SessionPool {
     return { added: false, reason: "NoSessionId" };
   }
 
+  // Return the info for a session running in this pool
   getSession(sessionId) {
     for (var session of this.sessions)
       if (session.sessionId === sessionId) return session;
@@ -68,7 +72,10 @@ class SessionPool {
     return removed;
   }
 
+  // Handle messages sent from the main thread, posting
+  // appropriate responses
   handleParentRequest(message) {
+    console.log("parent req: " + message);
     switch (message.request) {
       case "isFull":
         parentPort.postMessage({
@@ -80,6 +87,12 @@ class SessionPool {
       case "addSession":
         console.log("adding session");
         break;
+      case "poolInfo":
+        parentPort.postMessage({
+          responseTo: "poolInfo",
+          ...this.returnPoolInfo()
+        });
+        break;
     }
   }
 
@@ -90,7 +103,13 @@ class SessionPool {
   }
 
   isFull() {
-    return this.currentSessionsCount >= this.poolLimit;
+    // Checks to see if the pool is at its limits
+    return (this.full = this.currentSessionsCount >= this.poolLimit).full;
+  }
+
+  returnPoolInfo() {
+    // Add additional pool information here
+    return { isFull: this.isFull(), pool: this };
   }
 
   getPoolStrain() {
--- src/gameConnection/sessionsManager.js
+++ src/gameConnection/sessionsManager.js
@@ -17,9 +17,12 @@ A session is a running instance of a game
 
 */
 
-const sessionPools = new Map();
+// Pools running on worker threads
 const poolWorkers = new Map();
+
+// Total sessions running across all pools
 const sessions = new Map();
+
 let runningSessions = 0;
 
 // The runningPools count will be the same value as the runningWorkers
@@ -35,23 +38,29 @@ function endSession(sessionId) {
     }
   }
 }
+createPool(0, 5, 5);
 
 // Adds a session to the first free spot in a pool
 function addSession(sessionInfo) {
   debug("Registering new game session: " + JSON.stringify(sessionInfo));
 
-  for (var pool of sessionPools) {
-    // Adds to the first pool with space, should add some better load balancing
-    if (!pool.isFull()) {
-      debug(`Adding new session: ${session.sessionId} to pool: ${pool.poolId}`);
-      pool.registerSession(session);
-      runningSessions++;
-      return session;
-    }
-  }
+  //   updatePoolInfo().then(() => {
+  //     for (let pool in poolWorkers) {
+  //       // Add session to first open pool
+  //       pool.isFull;
+  //     }
+  //     debug(`Adding new session: ${session.sessionId} to pool: ${pool.poolId}`);
+  //     pool.registerSession(session);
+  //     runningSessions++;
+  //     return session;
+  //   });
 
   // No acceptable pool found, create a new pool with a worker to run it
   // Setting pool limit to 5 for testing, setting id to the current number of running sessions
+}
+
+// Create a new worker/pool and git it the new session info
+function createPool(poolId, poolLimit, sessionInfo) {
   debug(
     `Creating new session pool, id: ${runningSessions} session limit: ${5}`
   );
@@ -64,16 +73,6 @@ function addSession(sessionInfo) {
     }
   });
 
-  // worker.on("message", sessionPool => {
-  //   // Once the worker creates the new session pool, add it to the master list
-
-  //   debug(
-  //     `Adding new session pool: ${runningPools} to pool: ${sessionPool.poolId}`
-  //   );
-
-  // Testing is full
-  worker.on("message", full => console.log("full: " + JSON.stringify(full)));
-
   runningPools++;
   runningSessions++;
 
@@ -83,15 +82,10 @@ function addSession(sessionInfo) {
     return;
   });
 
-  worker.postMessage({ request: "isFull" });
   // Add worker to master list, number of workers == number of pools
   poolWorkers.set(worker.threadId, worker);
 
-  setInterval(() => {
-    for (let work of poolWorkers.values()) {
-      work.postMessage("isFull");
-    }
-  }, 5000);
+  updatePoolInfo();
 }
 
 // Returns object with data on current sessions
@@ -100,6 +94,21 @@ function getSessions() {
     count: runningSessions,
     sessions: sessions
   };
+}
+
+// Updates pool info in local variables, why not just reference the objects?
+// because... Tough times
+function updatePoolInfo() {
+  return new Promise(function(resolve, reject) {
+    for (let worker of poolWorkers.values()) {
+      console.log("Updating pool info");
+      worker.on("message", message => {
+        console.log("new info: " + JSON.stringify(message));
+        resolve();
+      });
+      worker.postMessage({ request: "poolInfo" });
+    }
+  });
 }
 
 // Merge pools is a manual call to compress pools
