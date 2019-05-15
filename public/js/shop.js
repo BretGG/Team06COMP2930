f367e3fed4e2c04f6fac18eb9275cc06b57aed35
Stella Tran <stellatranthi@hotmail.com>
2019-05-15 00:01:34 -0700

    Created some background for shop

--- public/js/shop.js
+++ public/js/shop.js
@@ -3,4 +3,26 @@ $(document).ready(() => {
 	window.onload = function() {     
 		$('#avatar').toggleClass('bounceIn');
   };
+
+	$('#shopAvatar').click(() =>{
+		$('#shopPlatform').css("background-color", "rgba(255,255,255, 0.75)");
+		$('#shopBackground').css("background-color", "rgba(255,255,255, 0.75)");
+		$('#shopAvatar').css("background-color", "#9CD6D6");
+		
+	})  
+
+	$('#shopPlatform').click(() => {
+		$('#shopAvatar').css("background-color", "rgba(255,255,255, 0.75)");
+		$('#shopBackground').css("background-color", "rgba(255,255,255, 0.75)");
+		$('#shopPlatform').css("background-color", "#9CD6D6");
+		
+	})
+
+	$('#shopBackground').click(() => {
+		$('#shopPlatform').css("background-color", "rgba(255,255,255, 0.75)");
+		$('#shopAvatar').css("background-color", "rgba(255,255,255, 0.75)");
+		$('#shopBackground').css("background-color", "#9CD6D6");
+		
+	})
+
 });
