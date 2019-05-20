bb0fc72b5406ea9f941f677c5bc3fd8675d5f541
BretGG <Bret.G@hotmail.ca>
2019-05-19 22:23:25 -0700

    Fixed name not showing up on main page after not loggin in, data object syntax was wrong

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -1,72 +1,68 @@
 $(document).ready(() => {
-
-    // setting encrypted and secure user token
-    $.ajaxSetup({
-        headers: {
-            'auth-token': localStorage.getItem('auth-token')
-        }
-    });
-
-    /** On page load, plays avatar animation */
-    window.onload = function() {
-        $('#avatar').toggleClass('bounceIn');
-    };
-
-
-    function getUserInfo(callback) {
-        console.log('sign in');
-        $.ajax({
-            type: 'get',
-            url: '/login/me',
-            success: function(data) {
-                console.log(data);
-                callback(data.user)
-            },
-            error: function(e) {
-                console.log(e.responseText);
-                callback("Unknown");
-            }
-        });
+  // setting encrypted and secure user token
+  $.ajaxSetup({
+    headers: {
+      "auth-token": localStorage.getItem("auth-token")
     }
+  });
 
+  /** On page load, plays avatar animation */
+  window.onload = function() {
+    $("#avatar").toggleClass("bounceIn");
+  };
 
-    /** Grabs user's username and appends to it welcome text */
-    function setProfileInfo(user) {
-        let welcome = $('#title');
-        welcome.text("Welcome, " + user.username + "!");
-    }
-
-
-    /** Calling setProfileInfo function */
-    getUserInfo(setProfileInfo);
-
-    /** Takes user to create room page */
-    $("#create").click(() => {
-        window.location.href = "createRoom";
-    })
-
-    /** Takes user to join room page */
-    $("#join").click(() => {
-        window.location.href = "joinRoom";
-    })
-
-    /** Takes user to join room page */
-    $("#cards").click(() => {
-        window.location.href = "mycard";    
-    })
-
-    /** Takes user back to signup/signin page */
-    $("#logout").click(() => {
-        window.location.href = "/";
+  function getUserInfo(callback) {
+    console.log("sign in");
+    $.ajax({
+      type: "get",
+      url: "/login/me",
+      success: function(data) {
+        console.log(data);
+        callback(data);
+      },
+      error: function(e) {
+        console.log(e.responseText);
+        callback("Unknown");
+      }
     });
+  }
 
-    /** Takes user back to shop page */
-    $("#shop").click(() => {
-        window.location.href = "/shop";
-    });
+  /** Grabs user's username and appends to it welcome text */
+  function setProfileInfo(user) {
+    let welcome = $("#title");
+    welcome.text("Welcome, " + user.username + "!");
+  }
 
-    /** Takes user back to mycard page */
-    $("#myCards").click(() => {
-        window.location.href = "/mycard";
-    });
+  /** Calling setProfileInfo function */
+  getUserInfo(setProfileInfo);
+
+  /** Takes user to create room page */
+  $("#create").click(() => {
+    window.location.href = "createRoom";
+  });
+
+  /** Takes user to join room page */
+  $("#join").click(() => {
+    window.location.href = "joinRoom";
+  });
+
+  /** Takes user to join room page */
+  $("#cards").click(() => {
+    window.location.href = "mycard";
+  });
+
+  /** Takes user back to signup/signin page */
+  $("#logout").click(() => {
+    window.location.href = "/";
+  });
+
+  /** Takes user back to shop page */
+  $("#shop").click(() => {
+    window.location.href = "/shop";
+  });
+
+  /** Takes user back to mycard page */
+  $("#myCards").click(() => {
+    window.location.href = "/mycard";
+  });
 });
