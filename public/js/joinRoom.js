ec49ca8f64405de0e3305ad1aa6ffcc4f3608f0b
BretGG <Bret.G@hotmail.ca>
2019-05-21 18:11:29 -0700

    Wrote AJAX call to join a rooom

--- public/js/joinRoom.js
+++ public/js/joinRoom.js
@@ -1,11 +1,22 @@
 $(document).ready(() => {
-		/** Takes user gameLobby page */
-    $("#join").click(() => {
-    	window.location.href="gameLobby";
+  /** Takes user gameLobby page */
+  $("#join").click(() => {
+    $.ajax({
+      type: "put",
+      data: {
+        sessionId: $("#roomNo").val(),
+        sessionPass: $("#roomPass").val()
+      },
+      success: function(data) {},
+      error: function(err) {
+        M.toast({ html: err.responseText, classes: "red" });
+      }
     });
+    window.location.href = "gameLobby";
+  });
 
-    /** Takes user back to main menu */
-    $("#back").click(() => {
-    	window.location.href="main";
-		});
+  /** Takes user back to main menu */
+  $("#back").click(() => {
+    window.location.href = "main";
+  });
 });
