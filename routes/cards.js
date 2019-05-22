aba9ea7687e708be38197afaf0b878b8b1f5c03c
Hannah Chung <hyerang1205@gmail.com>
2019-05-22 12:52:56 -0700

    server is preparing for editing cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,69 +1,88 @@
 const router = require("express").Router();
 const debug = require("debug")("comp2930-team2:server");
-const { Card, validate } = require("../src/models/card");
+const {
+    Card,
+    validate
+} = require("../src/models/card");
 const _ = require("lodash");
 
 router.post("/", async (req, res) => {
-  var card = _.pick(req.body, [
-    "format",
-    "category",
-    "question",
-    "answer",
-    "deck"
-  ]);
-  debug("Request to create cards: " + JSON.stringify(card));
+    var card = _.pick(req.body, [
+        "format",
+        "category",
+        "question",
+        "answer",
+        "deck"
+    ]);
+    debug("Request to create cards: " + JSON.stringify(card));
 
-  // Check if valid card data
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+    // Check if valid card data
+    const {
+        error
+    } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  // Create new card and save the database
-  card = new Card(card);
-  await card.save();
+    // Create new card and save the database
+    card = new Card(card);
+    await card.save();
 
-  debug("Creating card: " + JSON.stringify(card));
-  res.send(_.pick(card, ["question", "answer"]));
+    debug("Creating card: " + JSON.stringify(card));
+    res.send(_.pick(card, ["question", "answer"]));
 });
 
 // To update card
-router.put("/", async (req, res) => {
-  let cardInfo = _.pick(req.params, [
-    "cardId",
-    "format",
-    "category",
-    "question",
-    "answer",
-    "deck"
-  ]);
-  console.log('putting starts with '+cardInfo);
-  let card = Card.findById(cardInfo.cardId);
-  if (!card) if (error) return res.status(400).send("No card by that id");
+router.put("/:cardId", async (req, res) => {
+    let cardId = req.params.cardId;
+    // let cardFormat = req.body.format;
+    // let cardCategory = req.body.category;
+    // let cardQuestion = req.body.question;
+    // let cardAnswer = req.body.answer;
+    // let cardDeck = req.body.deck;
+    // console.log(cardId+": " + cardFormat+", "+cardCategory+", "+cardQuestion+", "+cardAnswer+", "+cardDeck);
 
-  card.update({
-    format: cardInfo.format,
-    category: cardInfo.category,
-    question: cardInfo.question,
-    answer: cardInfo.answer ? cardInfo.answer : card.answer,
-    deck: cardInfo.deck ? cardInfo.deck : card.deck
-  });
-  console.log(card);
+    let cardInfo = _.pick(req.body, [
+        "format",
+        "category",
+        "question",
+        "answer",
+        "deck"
+    ]);
+    console.log(cardInfo.answer);
+
+    let card = await Card.findById(req.params.cardId);
+    console.log(card);
+
+    if (!card)
+        if (error) return res.status(400).send("No card by that id");
+    console.log(cardInfo.answer ? cardInfo.answer : card.answer);
+
+    Card.update({_id: cardId},{ $set: {
+        format: cardInfo.format,
+        category: cardInfo.category,
+        question: cardInfo.question,
+        answer: cardInfo.answer ? cardInfo.answer : card.answer,
+        deck: cardInfo.deck ? cardInfo.deck : card.deck
+    }});
+    // await card.save();
+    console.log("Updated " + card);
+    // res.send(card);
 });
 
 
 // to delete card
 router.delete("/:cardId", async (req, res) => {
-    console.log('router.delete starts');
     let cardId = req.params.cardId;
-  let card = await Card.findById(cardId);
-  console.log(card);
+    let card = await Card.findById(cardId);
+    console.log(card);
 
-  // TODO: check if they are the owner
-  if (!card) if (error) return res.status(400).send("No card by that id");
+    // TODO: check if they are the owner
+    if (!card)
+        if (error) return res.status(400).send("No card by that id");
 
-  card = await Card.deleteOne({
-    _id: cardId
-  });
-  res.status(200).send(card);
+    card = await Card.deleteOne({
+        _id: cardId
+    });
+    res.status(200).send(card);
 });
 
 // get the cards
